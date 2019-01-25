939383951653ffe74939e2ce006a6f5f115aaeac
davidvink7 <35492389+davidvink7@users.noreply.github.com>
2019-01-24 21:25:09 -0500

    Indentation fixes

--- async_model_transactions.js
+++ async_model_transactions.js
@@ -1,38 +1,37 @@
 AtmlRecipeModel.findRecipesPaged = function(page, pageSize, callback){
 		
-	sqldataadapter.createTransaction(function(err, transaction){
-		if(err){
-			callback(err);
-			return;
-		}
+   sqldataadapter.createTransaction(function(err, transaction){
+      if(err){
+         callback(err);
+         return;
+      }
 		
-		async.series({
-			rows: function(callback){
-				var query = 'SELECT * FROM `atml_recipe` LIMIT ' + (pageSize * (page - 1)) + ', ' + pageSize;
-				transaction.executeQuery(query, callback);
+      async.series({
+         rows: function(callback){
+            var query = 'SELECT * FROM `atml_recipe` LIMIT ' + (pageSize * (page - 1)) + ', ' + pageSize;
+            transaction.executeQuery(query, callback);
 			},
-			rowCount: function(callback){
-				query = 'SELECT COUNT(*) AS totalRecords FROM atml_recipe';
-				transaction.executeQuery(query, callback);
-			}
-		}, function(err, results){
-			transaction.finish();
-			if(err){
-				callback(err);
-				return;
-			} else {
-				var resultMap = {
-					recipes: results.rows,
-					recordCount: results.rowCount[0]['totalRecords'],
-					page: page,
-					pageCount: Math.ceil(results.rowCount[0]['totalRecords'] / pageSize)
-				}
-				callback(resultMap);
-				return;
-			}
-		});
+         rowCount: function(callback){
+            query = 'SELECT COUNT(*) AS totalRecords FROM atml_recipe';
+            transaction.executeQuery(query, callback);
+         }
+      }, function(err, results){
+            transaction.finish();
+            if(err){
+               callback(err);
+            return;
+            } else {
+               var resultMap = {
+               recipes: results.rows,
+               recordCount: results.rowCount[0]['totalRecords'],
+               page: page,
+               pageCount: Math.ceil(results.rowCount[0]['totalRecords'] / pageSize)
+            }
+            callback(resultMap);
+            return;
+         }
+      });
 		
-		
-	});
+   });
 		
 };
