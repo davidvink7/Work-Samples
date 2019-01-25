1615462fb66be86a2a7063e9517b8e6750369887
davidvink7 <35492389+davidvink7@users.noreply.github.com>
2019-01-24 23:41:13 -0500

    Indentation

--- high_risk.js
+++ high_risk.js
@@ -2,16 +2,16 @@ var async = require("async");
 var mysql = require("./mysql_connect");
 
 exports.findUnit = (address,number,next) => {
-	async.waterfall([
-		(next) => {
-			mysql.connect_read(next);
-		},
-		(connection,next) => {
-			var query = "SELECT * FROM high_risk_units WHERE address = ? AND number = ? ORDER BY id DESC LIMIT 1";
-  		connection.query(query,[address,number],(err,rows) => {
-  			connection.destroy();
-				if(err) return next(err);
-				next(rows);
-  		});
-	}],next);
+   async.waterfall([
+      (next) => {
+         mysql.connect_read(next);
+      },
+      (connection,next) => {
+         var query = "SELECT * FROM high_risk_units WHERE address = ? AND number = ? ORDER BY id DESC LIMIT 1";
+         connection.query(query,[address,number],(err,rows) => {
+         connection.destroy();
+         if(err) return next(err);
+         next(rows);
+      });
+   }],next);
 };
