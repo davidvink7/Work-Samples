a7a4b60c14d84e49efa30283412922429875845c
davidvink7 <35492389+davidvink7@users.noreply.github.com>
2019-01-24 21:27:11 -0500

    Indentation fixes

--- batch_insert_and_update.js
+++ batch_insert_and_update.js
@@ -1,29 +1,28 @@
 AtmlRecipeModel.batchInsert = function(values, callback){
-	var query = "INSERT INTO `atml_recipe` (`name`, `url`, `brand_id`, `prefix`, `externalid`, `source`, `sourceid`, `created`, `edited`, `servings`) VALUES ?";
+   var query = "INSERT INTO `atml_recipe` (`name`, `url`, `brand_id`, `prefix`, `externalid`, `source`, `sourceid`, `created`, `edited`, `servings`) VALUES ?";
 
-	sqldataadapter.executeQueryWithParams(query, [values], function(err) {
-    if (err) {
-			throw err;
-		}
-		query = "SELECT LAST_INSERT_ID()";
+   sqldataadapter.executeQueryWithParams(query, [values], function(err) {
+      if (err) {
+         throw err;
+      }
+      query = "SELECT LAST_INSERT_ID()";
 		
-		sqldataadapter.executeQueryWithParams(query, [], function(err,result) {
-	    if (err) {
-				throw err;
-			}
-			var firstRecipeId = result[0]['LAST_INSERT_ID()'];
-			var recipeIds = _.range(firstRecipeId, firstRecipeId + values.length);
-			callback(recipeIds);
-		});
-	});
-
+      sqldataadapter.executeQueryWithParams(query, [], function(err,result) {
+         if (err) {
+            throw err;
+         }
+         var firstRecipeId = result[0]['LAST_INSERT_ID()'];
+         var recipeIds = _.range(firstRecipeId, firstRecipeId + values.length);
+         callback(recipeIds);
+      });
+   });
 };
 
 AtmlRecipeModel.batchUpdate = function(fileName, callback){
-	var queryForeignKeyChecksOff = 'SET foreign_key_checks = 0';
+   var queryForeignKeyChecksOff = 'SET foreign_key_checks = 0';
 
-	var query = '\
-		LOAD DATA LOCAL INFILE ' + mysql.escape(fileName) + '\
+   var query = '\
+                LOAD DATA LOCAL INFILE ' + mysql.escape(fileName) + '\
 		REPLACE\
 		INTO TABLE `atml_recipe`\
 		FIELDS TERMINATED BY ","\
@@ -31,15 +30,15 @@ AtmlRecipeModel.batchUpdate = function(fileName, callback){
 		LINES TERMINATED BY \'\\n\'\
 		IGNORE 1 ROWS';
 
-	var queryForeignKeyChecksOn = 'SET foreign_key_checks = 1';
+   var queryForeignKeyChecksOn = 'SET foreign_key_checks = 1';
 	
-	sqldataadapter.createTransaction(function(err,transaction){
-		if (err) {
-			throw err;
-		}
-		transaction.executeQuery(queryForeignKeyChecksOff);
-		transaction.executeQuery(query);
-		transaction.executeQuery(queryForeignKeyChecksOn);
-		transaction.finish(callback);
-	});
+   sqldataadapter.createTransaction(function(err,transaction){
+      if (err) {
+         throw err;
+      }
+      transaction.executeQuery(queryForeignKeyChecksOff);
+      transaction.executeQuery(query);
+      transaction.executeQuery(queryForeignKeyChecksOn);
+      transaction.finish(callback);
+   });
 };
