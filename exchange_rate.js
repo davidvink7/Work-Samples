290a056274ccea4881af9b198d8a37801984d895
davidvink7 <35492389+davidvink7@users.noreply.github.com>
2019-10-27 09:25:24 -0400

    create exchange_rate.js

---
+++ exchange_rate.js
@@ -0,0 +1,23 @@
+const _ = require("lodash");
+const mysql = require("mysql2");
+const mysql_connect = require("./mysql_connect");
+const utils = require("../../routes/v1/utils.js");
+const is_test = (process.env.NODE_ENV != "production");
+
+exports.getExchangeRate = async (code, callback) => {
+	let eur_code = utils.eu_country_codes(code);
+	if (!_.isUndefined(eur_code)) code = eur_code;
+	let query = "SELECT rate, symbol FROM exchange_rates WHERE symbol = ? OR country = ?";
+	let sql = mysql.format(query,[code,code]);
+
+	try {
+    let results = await mysql_connect.do_query(mysql_connect.promise.read,sql);
+    if(_.isFunction(callback)){
+      return callback(null,results);
+    } else return results;
+  } catch (err) {
+    if(_.isFunction(callback)){
+      return callback(err);
+    } else throw err;
+  }
+};
