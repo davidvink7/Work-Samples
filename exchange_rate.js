6d7304855a139c1dda1bb815dbd98d8b387f12d7
davidvink7 <35492389+davidvink7@users.noreply.github.com>
2019-10-27 09:27:18 -0400

    indentation

--- exchange_rate.js
+++ exchange_rate.js
@@ -5,19 +5,17 @@ const utils = require("../../routes/v1/utils.js");
 const is_test = (process.env.NODE_ENV != "production");
 
 exports.getExchangeRate = async (code, callback) => {
-	let eur_code = utils.eu_country_codes(code);
-	if (!_.isUndefined(eur_code)) code = eur_code;
-	let query = "SELECT rate, symbol FROM exchange_rates WHERE symbol = ? OR country = ?";
-	let sql = mysql.format(query,[code,code]);
+    let eur_code = utils.eu_country_codes(code);
+    if (!_.isUndefined(eur_code)) code = eur_code;
+    let query = "SELECT rate, symbol FROM exchange_rates WHERE symbol = ? OR country = ?";
+    let sql = mysql.format(query,[code,code]);
 
-	try {
-    let results = await mysql_connect.do_query(mysql_connect.promise.read,sql);
-    if(_.isFunction(callback)){
-      return callback(null,results);
-    } else return results;
-  } catch (err) {
-    if(_.isFunction(callback)){
-      return callback(err);
-    } else throw err;
-  }
+    try {
+        let results = await mysql_connect.do_query(mysql_connect.promise.read,sql);
+        if(_.isFunction(callback)) return callback(null,results);
+        else return results;
+    } catch (err) {
+        if(_.isFunction(callback)) return callback(err);
+        else throw err;
+    }
 };
